b4b688f3c6743da7d24d5da9d97a7b682cbb3816
Harapriya Mohapatra <jhanja2000@gmail.com>
2021-07-27 10:09:26 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import React from 'react';
+import './App.css';
+import Game from './components/Game'
+
+class App extends React.Component{
+  render() {
+  return (
+    <Game/>
+  );
+}
+}
+
+export default App;
